c1e306def124b54a2f62a39350ab0a1c04532666
hisui1998 <hisui4183@gmail.com>
2020-02-14 13:05:46 +0900

    更新

--- RayTracingPractice/main.cpp
+++ RayTracingPractice/main.cpp
@@ -122,7 +122,7 @@ float Dot(const float3& a, const float3& b)
 };
 
 
-/// オブジェクト類の当たり判定
+/// オブジェクト類
 // 球
 struct Sphere
 {
@@ -132,61 +132,100 @@ struct Sphere
 	Sphere(float rad) { this->rad = rad; pos = float3(); };
 	Sphere(float rad,float3 pos) { this->rad = rad; this->pos = pos; };
 };
+// 床
+struct Plane
+{
+	float offset;// 床がどのくらい浮いている(沈んでいる)のか
+	float3 normal;// 法線(正規化済み)
+	Plane() { offset = 0; normal = float3(0,1,0); };
+	Plane(float offset) { this->offset = offset; normal = float3(0,1,0); };
+	Plane(float offset, float3 normal) { this->offset = offset; this->normal = normal; };
+};
 
 
 // 球との当たり判定、当たった場所の色を返す。
-bool isHitSphere(float3& retCol, const float3& eye, const float3& ray, const Sphere& sph)
+bool isHitSphere(float3& retCol, const float3& eye, const float3& ray, const float3& light,const Sphere& sph)
 {
-	float3 toCenter = sph.pos - eye;// 視点から球体中心までのベクトルを出す
+	// 球体の中心点までのベクトル
+	float3 toCenter(sph.pos - eye);
+	// 球体とレイベクトルの接線のベクトルを算出
+	float3 RayVec(ray*Dot(toCenter,ray));
 
-	float CdotR = Dot(toCenter, ray);// 視点から球体中心までのベクトルと視線ベクトルの内積を取得
+	float3 line = toCenter - RayVec;
 
-	float3 vecR = ray * (toCenter * ray);
-
-	//視線ベクトルとベクトル長をかけて、中心からの垂線下した点を求めます
-	float3 line = toCenter - vecR;
-
-	// 中心からの垂線が半径より小さければ当たっている
-	if (line.Magnitude() <= sph.rad)
+	if (sph.rad >(toCenter- RayVec).Magnitude())
 	{
-		retCol = { 1,1,1 };
+		float cPos = sqrt(sph.rad * sph.rad - line.Magnitude() * line.Magnitude());
+
+		float distance = RayVec.Magnitude() - cPos;
+
+		float3 hitPos = eye + ray * distance;// 視線ベクトルと視点と距離から当たっている座標を取得
+
+		// 法線を出して正規化する
+		float3 normal = sph.pos- hitPos;
+		normal = normal.Normalized();
+
+		float shadow = Dot(light, normal);
+
+		shadow = max(min(shadow,1),0);
+		retCol = float3(1,1,1)*shadow;
 		return true;
 	}
+
 	return false;
 }
 
-void RayTracing(const float3& eye, const Sphere& sph)
+// 床との当たり判定、当たった場所の色を返す。
+bool isHitPlane(float3& retCol, const float3& eye, const float3& ray, const float3& light, const Plane& plane)
+{
+	return false;
+}
+
+// レイトレーシング本体
+void RayTracing(const float3& eye, const Sphere& sph,const Plane& plane)
 {
 	for (int y = 0; y < ScreenY; ++y)
 	{
 		for (int x = 0; x < ScreenX; ++x)
 		{
 			// 中心原点のスクリーン座標を取得
-			float3 screenPos(x, y, 0);
+			float3 screenPos(x- ScreenX/2, -y+ ScreenY/2, 0);
+			float3 light(1,-1,-1);
+			light = light.Normalized();
 
 			// 返り値用
 			float3 color;
 			float3 ray = screenPos - eye;
 			ray = ray.Normalized();
 
-			isHitSphere(color, eye, ray, sph);
+			// 当たり判定の取得
+			if (!isHitSphere(color, eye, ray, light, sph) && !isHitPlane(color, eye, ray, light, plane))
+			{
+				// レイが当たってなかったときは灰色にする
+				color = float3(0.25,0.25,0.25);
+			}
 
-			DrawPixel(x, y, GetColor(color.x, color.y, color.z));
+			DrawPixel(x, y, GetColor(color.x * 0xff, color.y * 0xff, color.z * 0xff));
 		}
 	}
 }
 
 int main()
 {
-	SetGraphMode(ScreenX, ScreenY,64);
+	SetGraphMode(ScreenX, ScreenY, 32);
 	ChangeWindowMode(true);
+	SetDrawScreen(DX_SCREEN_BACK);
 	DxLib_Init();
+
 	const float3 eye(0,0,300);// 視点
-	const Sphere sph(100,float3(0,0,0));
+	const Sphere sph(100,float3(0,0,0));// 球体
+	const Plane plane;// 床
+
+	// 実行ループ
 	while (!ProcessMessage()&&!CheckHitKey(KEY_INPUT_ESCAPE))
 	{
 		ClearDrawScreen();
-		RayTracing(eye, sph);
+		RayTracing(eye, sph, plane);
 		ScreenFlip();
 	}
 
